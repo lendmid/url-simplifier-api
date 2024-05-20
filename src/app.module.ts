590d4f5c5b24d1760602cbdad1bfc4609bd501be
Dmitrii Doroshenko <lendmid@gmail.com>
2024-05-20 21:47:33 +0300

    move DB to separate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlModule } from './url/url.module';
 import * as dotenv from 'dotenv';
+import { DatabaseModule } from './database/database.module';
 
 const environment = process.env.NODE_ENV || 'development';
 dotenv.config({ path: `.env.${environment}` });
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'urls.sqlite',
-      autoLoadEntities: true,
-      synchronize: environment === 'development',
-      migrationsRun: true,
-      // dropSchema: true,
-    }),
-    UrlModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UrlModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        type: 'sqlite',
+        database: 'urls.sqlite',
+        logging: process.env.NODE_ENV === 'development',
+        // synchronize: process.env.NODE_ENV === 'development',
+        synchronize: false,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
