1b76a3b9c363d069d536bf19c8ba1b72731acc4a
Dmitrii Doroshenko <lendmid@gmail.com>
2024-05-25 21:57:19 +0300

    add Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,25 @@ import { ConfigModule } from '@nestjs/config';
 import { UrlModule } from './url/url.module';
 import * as dotenv from 'dotenv';
 import { DatabaseModule } from './database/database.module';
+import Joi from 'joi';
 
 const environment = process.env.NODE_ENV || 'development';
 dotenv.config({ path: `.env.${environment}` });
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(4000),
+        NODE_ENV: Joi.string().default('development'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
     DatabaseModule,
     UrlModule,
   ],
