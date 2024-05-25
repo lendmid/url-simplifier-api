c451580e7f5efc671b60a32e83b8420bddb43bd2
Dmitrii Doroshenko <lendmid@gmail.com>
2024-05-25 20:17:10 +0300

    add rate limit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as process from 'process';
 import { useContainer } from 'class-validator';
 import { setupSwagger } from './configs/swagger';
+import rateLimit from 'express-rate-limit';
 
 const { PORT } = process.env;
 
@@ -16,6 +17,16 @@ async function bootstrap() {
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
+    }),
+  );
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 minutes
+      max: 100, // Limit each IP to 100 requests per `window`
+      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+      legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+      skipSuccessfulRequests: false,
+      message: { message: 'Too many requests', statusCode: 403 },
     }),
   );
 
