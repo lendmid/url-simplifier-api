be1a9a791ce48734c9982c95fc9cf0a1dcd4aed2
Dmitrii Doroshenko <lendmid@gmail.com>
2024-05-28 13:59:21 +0300

    delete rateLimit for dev

--- src/main.ts
+++ src/main.ts
@@ -26,16 +26,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  app.use(
-    rateLimit({
-      windowMs: 10 * 60 * 1000, // 10 minutes
-      max: 100, // Limit each IP to 100 requests per `window`
-      standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-      legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
-      skipSuccessfulRequests: false,
-      message: { message: 'Too many requests', statusCode: 403 },
-    }),
-  );
+  if (process.env.NODE_ENV !== 'development') {
+    app.use(
+      rateLimit({
+        windowMs: 10 * 60 * 1000, // 10 minutes
+        max: 100, // Limit each IP to 100 requests per `window`
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+        skipSuccessfulRequests: false,
+        message: { message: 'Too many requests', statusCode: 403 },
+      }),
+    );
+  }
 
   setupSwagger(app);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
